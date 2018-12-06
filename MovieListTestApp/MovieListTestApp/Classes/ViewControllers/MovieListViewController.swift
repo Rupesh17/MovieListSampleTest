4d36dff4e3e89e21f885941246500ac065b071f4
Rupesh Kumar <rupesh.kumar@nagarro.com>
2018-12-06 22:26:47 +0530

    Added project. Added folder structure.

---
+++ MovieListTestApp/MovieListTestApp/Classes/ViewControllers/MovieListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MovieListViewController.swift
+//  MovieListTestApp
+//
+//  Created by Rupesh Kumar on 06/12/18.
+//  Copyright © 2018 Rupesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
